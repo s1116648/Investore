4e3dd88515c58dea28048021c7210355d54d588b
Carl <carlzee@live.nl>
2022-01-23 00:25:14 +0100

    cleaned ItemsFragment.java

--- app/src/main/java/nl/hsleiden/investore/ui/items/ItemsFragment.java
+++ app/src/main/java/nl/hsleiden/investore/ui/items/ItemsFragment.java
@@ -4,23 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-import nl.hsleiden.investore.R;
-import nl.hsleiden.investore.data.GenerateExampleItems;
+
 import nl.hsleiden.investore.data.database.InvestoreDB;
 import nl.hsleiden.investore.data.model.Item;
 import nl.hsleiden.investore.databinding.FragmentItemsBinding;
@@ -41,7 +32,6 @@ public class ItemsFragment extends Fragment {
         itemsList = new ArrayList<>();
 
         recyclerView = binding.recyclerView;
-//        setItemInfo();
 
         loadDatabase();
         setItemInfoFromDB();
@@ -62,13 +52,6 @@ public class ItemsFragment extends Fragment {
         RecyclerAdapter recyclerAdapter = new RecyclerAdapter();
         binding.recyclerView.setAdapter(recyclerAdapter);
         recyclerAdapter.updateItemList(itemsList);
-    }
-
-    private void setItemInfo() {
-        for (int i = 0; i < GenerateExampleItems.getNumberOfCases(); i++) {
-            Item item = GenerateExampleItems.generateItem(i);
-            itemsList.add(item);
-        }
     }
 
     private void setItemInfoFromDB() {
