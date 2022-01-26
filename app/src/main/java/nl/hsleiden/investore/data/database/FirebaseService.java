b57ccd0b2c342049dff8cae80ee4538bacca784a
Carl <carlzee@live.nl>
2022-01-26 16:38:35 +0100

    Basic read is working

--- app/src/main/java/nl/hsleiden/investore/data/database/FirebaseService.java
+++ app/src/main/java/nl/hsleiden/investore/data/database/FirebaseService.java
@@ -1,7 +1,14 @@
 package nl.hsleiden.investore.data.database;
 
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseService {
 
@@ -14,5 +21,22 @@ public class FirebaseService {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
